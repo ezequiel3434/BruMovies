35ddd12809c8199ceac58c5fd0917527e1a0d425
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-02 12:29:30 -0300

    finishing UserDefaultsManager

---
+++ BruMovies/Code/Services/UserDefaultsManager.swift
@@ -0,0 +1,90 @@
+//
+//  UserDefaultsManager.swift
+//  BruMovies
+//
+//  Created by Ezequiel Parada Beltran on 02/11/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    
+    //MARK: - getter functions
+
+    func getNowPlayingList() -> [String] {
+        guard let ids = UserDefaults.standard.array(forKey: "NowPlaying") as? [String] else { return [] }
+        return ids
+    }
+
+    func getComingSoonList() -> [String] {
+        guard let ids = UserDefaults.standard.array(forKey: "ComingSoon") as? [String] else { return [] }
+        return ids
+    }
+
+    func getSubcriptionsList() -> [String] {
+        guard let ids = UserDefaults.standard.array(forKey: "Subcriptions") as? [String] else { return [] }
+        return ids
+    }
+    
+    //MARK: - setter functions
+    
+    func setNowPlayingList(titles: [String]) {
+        UserDefaults.standard.set(titles, forKey: "NowPlaying")
+    }
+    
+    func setComingSoonList(titles: [String]) {
+        UserDefaults.standard.set(titles, forKey: "ComingSoon")
+    }
+    
+    func setSubcriptionsList(titles: [String]){
+        UserDefaults.standard.set(titles, forKey: "Subcriptions")
+    }
+    
+    //MARK: - Subcriptions
+    
+    @discardableResult
+    func toggleSubcriptions(id: String) -> Bool {
+        let subcriptions = getSubcriptionsList()
+        
+        if subcriptions.contains(id) {
+            self.removeFromSubcriptions(id: id, subcriptions: subcriptions)
+            return false
+        } else {
+            self.addToSubcriptions(id: id, subcriptions: subcriptions)
+        }
+    }
+    
+    
+    @discardableResult
+    func removeFromSubcriptions(id: String, subcriptions: [String]) -> Bool {
+        self.setSubcriptionsList(titles: subcriptions.filter{$0 != id})
+        return true
+    }
+    
+    @discardableResult
+    func addToSubcriptions(id: String, subcriptions: [String]) -> Bool {
+        var newSubcriptions = subcriptions
+        newSubcriptions.append(id)
+        self.setSubcriptionsList(titles: newSubcriptions)
+        return true
+    }
+    
+    func checkIfSubcription(id: String) -> Bool {
+        let subcriptions = getSubcriptionsList()
+        if subcriptions.contains(id) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func clearUserDefaults() {
+        UserDefaults.resetStandardUserDefaults()
+    }
+    
+    
+}
+
+
+
