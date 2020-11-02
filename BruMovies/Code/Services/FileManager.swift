6e3b46e32deaa8fa2a855c75badb77832a089b16
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-02 12:55:11 -0300

    finishing FileManager

---
+++ BruMovies/Code/Services/FileManager.swift
@@ -0,0 +1,34 @@
+//
+//  FileManager.swift
+//  BruMovies
+//
+//  Created by Ezequiel Parada Beltran on 02/11/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+
+struct FileHandler {
+    let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    
+    func getPathForImage(id: String) -> URL {
+        let pathComponent = "\(id).jpg"
+        return documentsDirectory.appendingPathComponent(pathComponent)
+    }
+    
+    func checkIfFileExists(id: String) -> Bool {
+        let pathComponent = "\(id).jpg"
+        return FileManager.default.fileExists(atPath: documentsDirectory.appendingPathComponent(pathComponent).path)
+    }
+    
+    func flushDocumentsDirectory() -> Bool {
+        guard let paths = try? FileManager.default.contentsOfDirectory(at: self.documentsDirectory, includingPropertiesForKeys: nil) else { return false }
+        
+        for path in paths {
+            try? FileManager.default.removeItem(at: path)
+        }
+        return true
+    }
+    
+    
+}
